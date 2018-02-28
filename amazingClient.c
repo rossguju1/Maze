37fb2a05b53831a96d6267c5dff13f4181de6775
Rachel Martin <rachel.h.martin.19@dartmouth.edu>
2018-02-27 20:40:37 -0500

    with server communication

--- amazingClient.c
+++ amazingClient.c
@@ -1,5 +1,5 @@
 /* 
- * amazingClient - 
+ * amazingClient - Connects to the server and starts each thread to represent Avatars and solve the maze 
  *
  * 
  * usage: ./amazingClient Hostname 17235 nAvatars Difficulty logname user
@@ -11,25 +11,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>	      // read, write, close
-#include <strings.h>	      // bcopy, bzero
-#include <netdb.h>	      // socket-related structures
+#include <unistd.h>       // read, write, close
+#include <strings.h>        // bcopy, bzero
+#include <netdb.h>        // socket-related structures
 #include <time.h>
 #include "amazing.h"
 #include "startup.h"
 
 /**************** file-local constants ****************/
 
+int sendMessage(char* hostname, uint32_t MazePort, AM_Message* message);
+AM_Message* recieveMessage(int sock) ;
 /**************** main() ****************/
 int
 main(const int argc, char *argv[])
 {
-  char *program;	      // this program's name
-  char *hostname;	      // server hostname
+  char *program;        // this program's name
+  char *hostname;       // server hostname
   char* user;
-  int port;		      // server port
+  int port;         // initial server port
   int numAva;
   int diff;
+  AM_Message* initRecvMessage;
   AM_Message* recievedMessage;
   FILE* log;
   uint32_t MazePort;
@@ -51,18 +54,18 @@ main(const int argc, char *argv[])
   }
   log = fopen(argv[5], "w");
   if(log == NULL) {
-  	fprintf(stderr, "Could not open log\n");
-  	exit(1);
+    fprintf(stderr, "Could not open log\n");
+    exit(1);
   }
 
-  recievedMessage = AM_connect(hostname, port, numAva, diff);
-  if(recievedMessage != NULL) {
-    if(ntohl(recievedMessage->type) == AM_INIT_OK) {
-      MazePort = ntohl(recievedMessage->init_ok.MazePort);
-      MazeWidth = ntohl(recievedMessage->init_ok.MazeWidth);
-      MazeHeight = ntohl(recievedMessage->init_ok.MazeHeight);
+  initRecvMessage = AM_connect(hostname, port, numAva, diff);
+  if(initRecvMessage != NULL) {
+    if(ntohl(initRecvMessage->type) == AM_INIT_OK) {
+      MazePort = ntohl(initRecvMessage->init_ok.MazePort);
+      MazeWidth = ntohl(initRecvMessage->init_ok.MazeWidth);
+      MazeHeight = ntohl(initRecvMessage->init_ok.MazeHeight);
     }
-    free(recievedMessage);
+    free(initRecvMessage);
   }
   else {
     fprintf(stderr, "Could not initialize Maze\n");
@@ -74,9 +77,100 @@ main(const int argc, char *argv[])
 
  //THREADS
 
+  switch(ntohl(recievedMessage->type)) {
+    case AM_AVATAR_TURN:
+      //update grpah;
+      break;
+    case AM_MAZE_SOLVED:
+      //append to log it was solved;
+      break;
+    case AM_NO_SUCH_AVATAR:
+      //
+      break;
+    case AM_UNKNOWN_MSG_TYPE:
+      fprintf(stderr, "Server recieved an unknown message of type %lu\n", (unsigned long)ntohl(recievedMessage->AM_UNKNOWN_MSG_TYPE.BadType));
+      exit(3);
+      break;
+    case AM_UNEXPECTED_MSG_TYPE:
+      fprintf(stderr, "Unexpected Message\n");
+      break;
+    case AM_AVATAR_OUT_OF_TURN:
+      fprintf(stderr, "Avatar requested a move out of order.\n");
+      break;
+    case AM_TOO_MANY_MOVES:
+      fprintf(stderr, "Out of turns\n");
+      exit(3);
+      break;
+    case AM_SERVER_TIMEOUT:
+      fprintf(stderr, "Server timed out\n");
+      exit(3);
+      break;
+    case AM_SERVER_DISK_QUOTA:
+      fprintf(stderr, "Server encountered a disk quota error.\n");
+      break;
+    case AM_SERVER_OUT_OF_MEM:
+      fprintf(stderr, "Server was out of memory.\n");
+      exit(3);
+      break;
+  }
 
   fclose(log);
-  
-
 }
 
+int sendMessage(char* hostname, uint32_t MazePort, AM_Message* message) 
+{
+  AM_Message* toServer = message;
+ 
+  struct hostent *hostp = gethostbyname(hostname);
+  if (hostp == NULL) {
+    fprintf(stderr, "startup: unknown host '%s'\n", hostname);
+    return -1;
+  }
+
+  // Initialize fields of the server address
+  struct sockaddr_in server;  // address of the server
+  server.sin_family = AF_INET;
+  bcopy(hostp->h_addr_list[0], &server.sin_addr, hostp->h_length);
+  server.sin_port = htons(MazePort);
+
+  // Create socket
+  int comm_sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (comm_sock < 0) {
+    perror("opening socket");
+    return -1;
+  }
+
+  // And connect that socket to that server   
+  if (connect(comm_sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
+    perror("connecting stream socket");
+    return -1;
+  }
+  printf("Connected!\n");
+
+  // 
+  if(send(comm_sock, toServer, sizeof(AM_Message), 0) == -1) {
+    fprintf(stderr, "Message could not be sent to the server\n");
+    return -1;
+  } else {
+    return comm_sock;
+  }
+}
+
+AM_Message* recieveMessage(int sock) 
+{
+  AM_Message* fromServer = malloc(sizeof(AM_Message));
+  
+  if(recv(comm_sock, fromServer, sizeof(AM_Message), 0) == -1) {
+    fprintf(stderr, "Message could not be recieved from the server\n");
+    close(comm_sock);
+    return NULL;
+  } else {
+      if(IS_AM_ERROR(ntohl(fromServer->type))) {
+       fprintf(stderr, "An %lu error was recieved from the server.\n", ntohl(fromServer->type));
+       return fromServer;
+       close(comm_sock);
+      } else {
+        return fromServer;
+    }
+  }
+}
